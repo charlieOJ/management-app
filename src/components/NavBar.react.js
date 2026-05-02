75aaf8234cfe1a594565af47841fc0fc5dac6f4f
C.O. Jansen <ophe.jansen@gmail.com>
2026-05-02 19:02:26 +0200

    Add projectForm and display projectsList in navbar

--- src/components/NavBar.react.js
+++ src/components/NavBar.react.js
@@ -1,11 +1,24 @@
-const NavBar = () => {
+const NavBar = ({ projects, addProject }) => {
   return (
-    <aside className="fixed pl-4 inset-y-0 z-40 flex place-items-left max-h-screen w-16 md:w-64 flex-shrink-0 transform flex-col overflow-hidden border-r shadow-lg transition-all border-gray-700 bg-gray-900 text-slate-50">
+    <aside className="px-4 flex place-items-left flex-col max-h-screen min-h-screen overflow-hidden border-gray-700 bg-gray-900 text-slate-50">
       <h1 className="text-xl py-2 mt-5 mb-5">YOUR PROJECTS</h1>
 
-      <button className="px-4 py-2 font-semibold bg-gray-700 text-slate-50 max-w-max">
+      <button
+        onClick={addProject}
+        className="px-4 py-2 font-semibold bg-gray-700 text-slate-50 max-w-max"
+      >
         + Add project
       </button>
+
+      {projects.length > 0 && (
+        <ul className="mt-5">
+          {projects.map((project, index) => (
+            <li key={index} className="py-2 border-t border-gray-700">
+              {project.title}
+            </li>
+          ))}
+        </ul>
+      )}
     </aside>
   );
 };
