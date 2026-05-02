a870d77c1abde686689f6697d6fa591946e4b688
C.O. Jansen <ophe.jansen@gmail.com>
2026-05-02 12:51:26 +0200

    add basic style and basics display conditions

---
+++ src/components/NavBar.react.js
@@ -0,0 +1,9 @@
+const NavBar = () => {
+  return (
+    <aside className="fixed inset-y-0 z-40 flex max-h-screen w-16 md:w-64 flex-shrink-0 transform flex-col overflow-hidden border-r shadow-lg transition-all border-gray-700 bg-gray-900 text-slate-50">
+      <h1>Title</h1>
+    </aside>
+  );
+};
+
+export default NavBar;
